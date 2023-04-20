cabbca1354904e4aeaee81880027f8b5e094c5aa
iplatos <platonoff9168@gmail.com>
2023-04-20 21:41:49 +0300

    fix: change react router dom version

--- src/App.tsx
+++ src/App.tsx
@@ -4,11 +4,11 @@ import Header from "./components/Header/Header";
 import Profile from "./components/Profile/Profile";
 import NavBar from "./components/NavBar/NavBar";
 import Dialogs from "./components/Dialogs/Dialogs";
-import {Route, Routes} from "react-router-dom";
 import {Music} from "./components/Musik/Music";
 import {Settings} from "./components/Settings/Settings";
 import {News} from "./components/News/News";
 import UsersContainer from "./components/Users/usersContainer";
+import {Route} from "react-router-dom";
 
 //57 start
 function App() {
@@ -20,14 +20,12 @@ function App() {
             <Header/>
             <NavBar/>
             <div className={"appWrapperContent"}>
-                <Routes>
-                    <Route path={"/messages/*"} element={<Dialogs/>}/>
-                    <Route path={"/profile"} element={<Profile/>}/>
-                    <Route path={"/news"} element={<News/>}/>
-                    <Route path={"/users"} element={<UsersContainer />}/>
-                    <Route path={"/music"} element={<Music/>}/>
-                    <Route path={"/settings"} element={<Settings/>}/>
-                </Routes>
+                    <Route path={"/messages/*"} component={()=><Dialogs/>}/>
+                    <Route path={"/profile"}  component={()=><Profile/>}/>
+                    <Route path={"/news"}  component={()=><News/>}/>
+                    <Route path={"/users"}  component={()=><UsersContainer />}/>
+                    <Route path={"/music"}  component={()=><Music/>}/>
+                    <Route path={"/settings"}  component={()=><Settings/>}/>
             </div>
 
 
